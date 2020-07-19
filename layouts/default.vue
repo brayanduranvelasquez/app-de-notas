103b9a7f355669c521fbc0c43765243df5140c39
Brayan DVelasquez <brayan13s133@gmail.com>
2020-07-18 22:41:48 -0430

    Propiedades para mejorar el scrollbar
    El scrollbar, es la barra lateral para subir y bajar en el sitio web. Ahora la vista default contiene propiedades css para la scrollbar. Tambien, para que no hayan desplazamientos bruscos, que sea util mas adelante cuando se tenga que hacer focus a un campo de formulario especifico.

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,21 @@
     padding: 0;
     box-sizing: border-box;
   }
+  
+  ::-webkit-scrollbar {
+    background: #ccc;
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #555;
+    border-radius: 40px;
+  }
 
   html {
     background: #fff;
     font-family: 'Lato';
+    scroll-behavior: smooth;
   }
 
 </style>
