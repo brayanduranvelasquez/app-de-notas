d7a755b1f9b76a63aa46c2826c3160665ee08129
Brayan DVelasquez <brayan13s133@gmail.com>
2020-07-21 21:29:04 -0430

    Arreglado comentario en la vista eliminar-nota
    El comentario hace mencion a que se puede eliminar una nota, gracias al parametro. Antes decir: para poder editar

--- pages/eliminar-nota/index.vue
+++ pages/eliminar-nota/index.vue
@@ -11,7 +11,7 @@
     },
 
     beforeCreate() {
-      // Ya que para poder editar una nota, se necesita de su indice (id), al entrar en el index, solo redireccionara al inicio
+      // Ya que para poder eliminar una nota, se necesita de su indice (id), al entrar en el index de esta vista, solo redireccionara al inicio
 
       this.$router.push('/');
     }
